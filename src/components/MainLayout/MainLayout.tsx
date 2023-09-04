8c2c645522bd5b3bc6309f23fdd7ef77633d417f
nandotmbn <orlandopratama695@gmail.com>
2023-09-04 12:59:28 +0700

    fix: add locale route

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -1,10 +1,11 @@
+"use client"
+
 import Link from "next/link";
 import React from "react";
 import LanguageSwitcher from "./partials/LanguageSwitcher";
 import ModeSwitcher from "./partials/ModeSwitcher";
-import { MenuOutlined } from "@ant-design/icons";
-import { Drawer } from "antd";
 import DrawerMenu from "./partials/DrawerMenu";
+import { usePathname } from "next/navigation";
 
 function MainLayout({
 	children,
@@ -13,11 +14,14 @@ function MainLayout({
 	children: React.ReactNode;
 	dictionary: any;
 }) {
+	const pathname = usePathname();
+	const locale = pathname.split("/")[1];
+
 	return (
 		<>
 			<header className="border-b-2 border-gray-300 dark:bg-gray-900">
 				<div className="flex flex-row h-16 items-center justify-between w-11/12 m-auto">
-					<Link href="/" className="center">
+					<Link href={"/" + locale} className="center">
 						<h1
 							style={{ fontFamily: "Bruno Ace" }}
 							className="text-gray-800 font-light text-sm dark:text-white"
@@ -30,14 +34,14 @@ function MainLayout({
 					<div className="flex-row gap-8 items-center hidden md:flex">
 						<ul className="flex flex-row gap-4 dark:invert">
 							<li>
-								<Link href="/favourites">
+								<Link href={"/" + locale + "/favourites"}>
 									<p className="text-sm text-gray-600 hover:text-black hover:dark:text-white dark:invert">
 										{dictionary?.headerMenu?.favourite}
 									</p>
 								</Link>
 							</li>
 							<li>
-								<Link href="/background">
+								<Link href={"/" + locale + "/background"}>
 									<p className="text-sm text-gray-600 hover:text-black hover:dark:text-white dark:invert">
 										{dictionary?.headerMenu?.background}
 									</p>
