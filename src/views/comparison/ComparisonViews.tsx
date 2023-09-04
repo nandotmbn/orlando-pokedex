ee963a9fcf8d852a58bc5de8396b1d488c59f4b6
nandotmbn <orlandopratama695@gmail.com>
2023-09-05 00:50:03 +0700

    fix: grid controll

--- src/views/comparison/ComparisonViews.tsx
+++ src/views/comparison/ComparisonViews.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, {useState, useEffect} from "react";
 import DetailComparison from "./partials/DetailComparison/DetailComparison";
 
 interface IComparisonViews {
@@ -11,9 +11,8 @@ interface IComparisonViews {
 function ComparisonViews({ dictionary, searchParams }: IComparisonViews) {
 	const pokemons = searchParams?.pokemon?.split(",");
 
-	const gridControl = `grid grid-cols-1 lg:grid-cols-${pokemons?.length}`;
 	return (
-		<div className={`${gridControl} justify-between items-start bg-gray-900`}>
+		<div className={`grid grid-cols-3 justify-between items-start bg-gray-900`}>
 			{pokemons.map((poke: string) => {
 				return (
 					<DetailComparison
